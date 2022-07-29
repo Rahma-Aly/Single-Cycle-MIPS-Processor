d2295da13c150589cadcf6cf29686a820441317c
Rahma-Aly <71734575+Rahma-Aly@users.noreply.github.com>
2022-07-29 18:41:24 +0200

    Update instr_memory.vhd

--- instr_memory.vhd
+++ instr_memory.vhd
@@ -11,27 +11,33 @@ end instr_memory;
 
 
 architecture dataflow of instr_memory is
-type memory is array(0 to 15) of std_logic_vector(31 downto 0);
+type memory is array(0 to 50) of std_logic_vector(7 downto 0);
 
-signal Instr_memory: memory:= (  x"01285024", --0x0040 0000: and 
-                                 x"018b6825", --0x0040 0004: or
-                                 x"01285020", --0x0040 0008: add
-                                 x"01285022", --0x0040 000c: sub
-                                 x"0149402a", --0x0040 0010: slt
-                                 x"1211fffb", --0x0040 0014: beq
-                                 x"01285024", --0x0040 0018: and
-                                 x"018b6825", --0x0040 001c: or
-                                 x"01285020", --0x0040 0020: add
-                                 x"01285022", --0x0040 0024: sub
-                                 x"0149402a", --0x0040 0028: slt
-                                 x"08100000", --0x0040 002c: j 0x0040 0000
-                                 x"00000000",
-                                 x"00000000",
-                                 x"00000000",
-                                 x"00000000"                            
+signal Instr_memory: memory:= (  -- and 
+                                0 => "00000000", 1 => "11100100",  2 => "01010000", 3 => "00100100",
+                                -- or
+                                4 => "00000001", 5 => "00110000",  6 => "01101000", 7 => "00100101", 
+                                -- add
+                                8 => "00000000", 9 => "10100001", 10 => "00011000",11 => "00100000", 
+                                -- sub
+                                12 => "00000000", 13 => "00100010", 14 => "00011000", 15 => "00100010", 
+                                -- slt
+                                16 => "00000010", 17 => "01110110", 18 => "10001000", 19 => "00101010",
+                                -- beq
+                                20 => "00010001", 21 => "00101011", 22 => "00000000", 23 => "00010100",
+                                --lw 
+                                24 => "10001100", 25 => "00000011", 26 => "00000000", 27 => "00000000", 
+                                 --sw
+                                28 => "10101100", 29 => "00000001", 30 => "00000000", 31 => "00000000",
+                                 --j 6
+                                32 => "00001000", 33 => "00000000", 34 => "00000000", 35 => "00000110",
+                                 others => "00000000"                         
                               );
 
 begin
-  Instr <= Instr_memory((to_integer(unsigned(Addr))));
+      Instr <= instr_memory(to_integer(unsigned(addr)))     &
+           instr_memory(to_integer(unsigned(addr) + 1)) &
+           instr_memory(to_integer(unsigned(addr) + 2)) &
+           instr_memory(to_integer(unsigned(addr) + 3));
 
 end architecture dataflow;

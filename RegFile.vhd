4852fdd09efa454b4c2e92f7fd3808305683304a
Rahma-Aly <71734575+Rahma-Aly@users.noreply.github.com>
2022-07-29 12:56:23 +0200

    Update RegFile.vhd

--- RegFile.vhd
+++ RegFile.vhd
@@ -12,7 +12,7 @@ Read_Reg1, Read_Reg2: in std_logic_vector (4 downto 0);
 Write_Reg: in std_logic_vector (4 downto 0);
  
 RegWrite : in std_logic; --write enable
-clk: in std_logic;
+clk, reset: in std_logic;
 
 RD1, RD2: out std_logic_vector (31 downto 0)
 );
@@ -22,11 +22,12 @@ architecture Behav of RegFile is
 type reg_type is array (0 to 31) of std_logic_vector(31 downto 0);
 signal register_array: reg_type;
 begin
-  process(CLK)
+  process(CLK, reset)
    begin
-  if (rising_edge(clk)) and (RegWrite  = '1')then
+     if reset = '1' then register_array(to_integer(unsigned(write_reg))) <= (others => '0');
+     elsif (rising_edge(clk)) and (RegWrite  = '1')then
       register_array (to_integer(unsigned(write_reg)))<= Wdata;
-  end if;
+     end if;
 end process;
 RD1 <= register_array(to_integer(unsigned(Read_Reg1)));
 RD2 <= register_array(to_integer(unsigned(Read_Reg2)));
